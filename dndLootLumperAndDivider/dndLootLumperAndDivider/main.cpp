9d5fc32ad5e9d36fcc1ce9df5191277f9c0d4fa5
Daegybyte <lucy@Anonymouss-MacBook-Pro.local>
2020-09-25 17:06:35 -0600

    minor cosmetic tweaks

--- dndLootLumperAndDivider/dndLootLumperAndDivider/main.cpp
+++ dndLootLumperAndDivider/dndLootLumperAndDivider/main.cpp
@@ -13,41 +13,42 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     
     bool y = true;
+    
     while (y) {
         
-//    This is the party loot divider programme
-    cout << "How many people are in your party" << endl;
+//This is the party loot divider programme
+    cout << "How many people are in your party? ";
     int partySize;
     cin >> partySize;
-    //coin inputs
-    cout << "\nHow much loot is being divided?" << endl;
-    cout << "\nPlatinum: " << endl;
+//coin inputs
+    cout << "\n\nHow much loot is being divided?" << endl;
+    cout << "\nPlatinum: ";
     int partyPlatinum;
     cin >> partyPlatinum;
-    cout << "Gold: " << endl;
+    cout << "Gold: ";
     int partyGold;
     cin >> partyGold;
-    cout << "Silver: " << endl;
+    cout << "Silver: ";
     int partySilver;
     cin >> partySilver;
-    cout << "Copper: " << endl;
+    cout << "Copper: ";
     int partyCopper;
     cin >> partyCopper;
 
-    //platinum
+//platinum
     int platinum = partyPlatinum;
 
-    //gross gold
+//gross gold
     int grossDividedGold = (partyGold + (platinum*10));
     int gold = grossDividedGold/partySize;
     int goldRemaining = grossDividedGold - (partySize*gold);
 
-    //gross silver
+//gross silver
     int grossDividedSilver = (partySilver + (goldRemaining*10));
     int silver = grossDividedSilver/partySize;
     int silverRemainder = grossDividedSilver - (partySize*silver);
 
-    //gross copper
+//gross copper
     int grossDividedCopper = (partyCopper + (silverRemainder*10));
     int copper = grossDividedCopper/partySize;
     int copperRemaining = grossDividedCopper - (partySize*copper);
@@ -58,11 +59,11 @@ int main(int argc, const char * argv[]) {
     cout << gold << "gp" << endl;
     cout << silver << "sp" << endl;
     cout << copper << "cp" << endl;
-    cout << "Copper left to go back to party Inventory: " << copperRemaining << "cp" << endl;
+    cout << "Amount remaining in party inventory: " << copperRemaining << "cp" << endl;
 
 
-    //This is the loot lumper
-    //Where the magic happens
+//This is the loot lumper
+//Where the magic happens
 
     int netCopper;
     netCopper = copper%10;
@@ -81,15 +82,15 @@ int main(int argc, const char * argv[]) {
 
     int netGold;
     netGold = gold + silverToGold;
-
-
-    cout << "\nCombined coins per player:" << endl;
+        
+    cout << "\nOr\n" << endl;
+    cout << "Combined coins per player:" << endl;
     cout << netGold << "gp" << endl;
     cout << netSilver << "sp" << endl;
     cout << netCopper << "cp" << endl;
-    cout << "Copper left to go back to party Inventory: " << copperRemaining << "cp\n" << endl;
+    cout << "Amount remaining in party inventory: " << copperRemaining << "cp\n" << endl;
     
-//    While loop check
+//While loop check
     cout << "Would you like to input more coins? y/n" <<  endl;
     char programmeContinue;
         cin >> programmeContinue;
