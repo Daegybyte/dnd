b01664c8f7394ae883f6cd273edfdff4c83ade17
Daegybyte <daegybyte@protonmail.ch>
2021-04-01 11:47:56 -0600

    Added suggested uses and comments for readability

--- pythonDnd/wabbajack.py
+++ pythonDnd/wabbajack.py
@@ -1,14 +1,22 @@
 import random
 
-polymorph = ["Chicken", "Goat", "Swarm of Bees!", "Kobold", "Rabbit", "Owl", "Stein of Ale", "T-Rex"]
+"""
+* The wabbajack ignores all resistances, willingness (polymorph),or saving throws unless otherwise specified.
+This is because random nature of the Wabbajack essentially functions as the saving throw. The fact that it did not roll polymorph means you have in a sense beaten a DC20 against it.
+* Evocation wizards who use the Wabbajack can sculpt evocation spells cast from it, but only for two creatures and it takes their reaction.
+* Wabbajack Juice is a potion that magically appears in the player inventory. The effects are chosen after consumption and are most likely a health potion, but can be a potion of hill giant strength or a potion of poison
+https://github.com/Daegybyte/dnd/blob/master/pythonDnd/wabbajackJuice.py
+"""
+
+polymorph = ["chicken", "goat", "swarm of bees!", "kobold", "rabbit", "owl", "stein of ale", "T-Rex"]
 buffs = ["enlarge", "reduce", "haste", "freedom of movement", "invisibility"]
 direction = ["in front of", "behind", "to the left of", "to the right of", "above"]
 person = ["Selsys", "Nestor", "Rafe", "Friday","Theodor","Sever", "the nearest enemy", "the farthest enemy"]
-
  
+wabbajackSelector = int(random.randint(0,17)) #for play set to 0,17 ......More effects should be added
 flipCoin = int(random.randint(0,1)) #for play set to 0,1
 rollD100 = int(random.randint(0,99)) #for play set to 0,19
-rollD20 = int(random.randint(2,2)) #for play set to 0,17 ......More effects should be added
+rollD20 = int(random.randint(1,20)) #for play set to 0,17 ......More effects should be added
 rollD10 = int(random.randint(1,10)) #for play set to 1,10
 rollD8 = int(random.randint(1,8)) #for play set to 1,8
 rollD5 = int(random.randint(1,5)) #for play set 1,5
@@ -19,39 +27,48 @@ rollDirection = int(random.randint(0,len(direction)-1))
 rollPerson = int(random.randint(0,len(person)-1))
 rollDistanceNear = int(random.randint(1,5))
 
-if rollD20 == 0:
+#fireball
+if wabbajackSelector == 0:
     print("A fireball shoots towards the nearest enemy who takes 2d10 damage")
-elif rollD20 == 1:
+#lightning bolt
+elif wabbajackSelector == 1:
     print("A lightning bolt shoots towards the nearest enemy who takes 3d6 damage")
-elif rollD20 == 2:
+#Ice storm
+elif wabbajackSelector == 2:
     flipCoin
     rollD10
     if flipCoin == 1:
         print(str("A ")+str(rollD10)+str(" square diameter ice storm centred on the nearest enemy and they take 4d6 damage"))
     else:
         print(str("A ")+str(rollD10)+str(" square diameter ice storm centred on the farthest enemy and they take 4d6 damage"))
-elif rollD20 == 3:
+#Heal
+elif wabbajackSelector == 3:
     flipCoin
     if flipCoin == 0:
         print("Heal self 10HP")
+    # maybe add a 1/100 chance to full heal party
     else:
         print("Nearest enemy heals 10HP")
-elif rollD20 == 4:
-    rollD20
-    print(str(int(rollD20+1))+str("gp falls from the sky"))
-elif rollD20 == 5:
+#Make it rain
+elif wabbajackSelector == 4:
+    wabbajackSelector
+    print(str(int(wabbajackSelector+1))+str("gp falls from the sky"))
+#Polymorph
+elif wabbajackSelector == 5:
     rollD10
     if rollD10 == 1:
         print(str("You have been turned into a ")+str(polymorph[rollPolymorph])+str(" for one minute"))
     else:
         print(str("Nearest enemy has been turned into a ")+str(polymorph[rollPolymorph])+str(" for one minute"))
-elif rollD20 == 6:
+#Invisibility
+elif wabbajackSelector == 6:
     flipCoin
     if flipCoin == 1:
         print("You are now invisible for one minute, or until you attack or until you take damage")
     else:
         print("Nearest enemy is now invisble for one minute, or until it attacks or takes damage")
-elif rollD20 == 7:
+#Summon Kobold
+elif wabbajackSelector == 7:
     rollD10
     if rollD10 == 1:
         print("An enemy kobold appears")
@@ -59,50 +76,59 @@ elif rollD20 == 7:
         print("A friendly kobold appears and aids you in combat for one day")
     else:
         print("A friendly kolbold appears and gives you 10 Silver and a pretty flower")
-elif rollD20 == 8:
+#Prone
+elif wabbajackSelector == 8:
     print("All creatures, monsters, etc. Are knocked prone")
-elif rollD20 == 9:
+#Wabbajack Juice 
+elif wabbajackSelector == 9:
     print("Wabbajack Juice has been added to your inventory \nThe Effects of Wabbajack juice can only be revealed after consumption.")
-elif rollD20 == 10:
+#Fear -- possible to become afraid of self. House rules are encouraged to figure this out.
+elif wabbajackSelector == 10:
     rollD10
     if rollD10 == 1:
         print(str("You are frightened of ")+str(person[rollPerson])+str(" for one minute"))
     else:
         print("Nearest enemy is now frightened of you for one minute")
-elif rollD20 == 11:
+#Exhaustion
+elif wabbajackSelector == 11:
     print("You take one level of exhaustion")
-elif rollD20 == 12:
+#Ballbearings
+elif wabbajackSelector == 12:
     rollBallbearings = int(random.randint(1,4))
     distanceBallbearings = int(random.randint(1,4))
     print(str(rollBallbearings)+str(" thousand ballbearings appear in ")+str(rollBallbearings)+str(" squares ")+str(distanceBallbearings)+str(" ")+str(direction[rollDirection])+str(" ")+str(person[rollPerson]))
-elif rollD20 == 13:
+#Absorb HP
+elif wabbajackSelector == 13:
     rollD5
     if rollD5 == 1:
         print("You have been sapped of 15hp and it has been given to the nearest enemy")
     else:
         print("You have absorbed 10hp from the nearest enemy")
-elif rollD20 == 14:
+#Buffs
+elif wabbajackSelector == 14:
     rollBuffs
     rollD10
     if rollD10 == 1:
         print(str(person[rollPerson])+str(" has been buffed with ")+str(buffs[rollBuffs]))
     else:
         print(str("You have been buffed with with ")+str(buffs[rollBuffs]))
-
-elif rollD20 == 15:
+#Cheese storm
+elif wabbajackSelector == 15:
     rollD100
     if rollD100 == 99:
         print("Blazing wheels of cheese plummet to the ground at four different points you can see within one mile. Each creature in a 40-foot-radius sphere centered on each point you choose must make a Dexterity saving throw.\nThe sphere spreads around corners.\nA creature takes 20d6 fire damage and 20d6 bludgeoning damage on a failed save, or half as much damage on a successful one.\nA creature in the area of more than one fiery burst is affected only once.The spell damages objects in the area and ignites flammable objects that aren't being worn or carried.")
     else:
         print("Cheese rains down from on high pummeling your foes in a 10 foot area within 90 feet with 3d6 bludgeoning damage , or half as much upon a successful Dex save 18+")
-elif rollD20 == 16:
+#Random Damage
+elif wabbajackSelector == 16:
     flipCoin
     rollD10
     if flipCoin == 0:
         print(str("You take ")+str(int(rollD10))+str(" damage"))
     else:
         print(str("The nearest enemy to you takes ")+str(int(rollD10))+str(" damage"))
-elif rollD20 == 17:
+#Explosion
+elif wabbajackSelector == 17:
     rollExplosionSize = int(random.randint(1,3))
     rollDistanceNear
     rollDirection
