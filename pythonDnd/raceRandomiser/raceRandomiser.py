8294e7f2619fb31f84ec8deb1ed8e8611e2de349
Daegybyte <daegybyte@protonmail.ch>
2021-04-10 19:53:50 -0600

    created json arrays for race and class

---
+++ pythonDnd/raceRandomiser/raceRandomiser.py
@@ -0,0 +1,55 @@
+import random
+
+rollRace = int(random.randint(0,len(race)-1))
+rollParty = int(random.randint(0,len(party)-1))
+
+party = ["Selsys", "Nestor", "Rafe", "Friday","Theodor","Sever"]
+race = [
+"Dragonborn",
+"Dwarf",
+"Elf",
+"Gnome",
+"Half-Elf",
+"Halfling",
+"Half-Orc",
+"Human",
+"Tiefling",
+"Changeling",
+"Kalashtar",
+"Shifter",
+"Warforged",
+"Aarakocra",
+"Genasi",
+"Goliath",
+"Centaur",
+"Loxodon",
+"Minotaur",
+"Simic Hybrid",
+"Vedalken",
+"Aetherborn",
+"Aven",
+"Khenra",
+"Kor",
+"Merfolk",
+"Naga",
+"Siren",
+"Vampire",
+"Aasimar",
+"Bugbear",
+"Firbolg",
+"Goblin",
+"Hobgoblin",
+"Kenku",
+"Kobold",
+"Lizardfolk",
+"Orc",
+"Tabaxi",
+"Triton",
+"Yuan-ti Pureblood",
+"Gith",
+"Locathah",
+"Tortle",
+"Verdan "
+]
+
+print(str(party[rollParty])+str(" has been turned into a ")+str(race[rollRace])) 
