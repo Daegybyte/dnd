e4b0339da6bf5ef5944ef9bb734ca012d2892022
Daegybyte <daegybyte@protonmail.ch>
2021-04-11 11:06:13 -0600

    new character generator to practice reading from .json

---
+++ pythonDnd/characterGen/characterGen.py
@@ -0,0 +1,13 @@
+import random
+import json
+
+name = json.loads(open('fantasyNameList.json').read())
+race = json.loads(open('racesDnd.json').read())
+charClass = json.loads(open('classesDnd.json').read())
+
+rollName = int(random.randint(0,len(name)-1))
+rollSurname = int(random.randint(0,len(name)-1))
+rollRace = int(random.randint(0,len(race)-1))
+rollClass = int(random.randint(0,len(charClass)-1))
+
+print(str("\na ")+str(race[rollRace])+str(" ")+str(charClass[rollClass])+str(" named ")+str(name[rollName])+str(" ")+str(name[rollSurname])+str(" "))
--- pythonDnd/raceRandomiser/raceRandomiser.py
+++ pythonDnd/raceRandomiser/raceRandomiser.py
@@ -1,55 +1,9 @@
 import random
+import json
 
-party = ["Selsys", "Nestor", "Rafe", "Friday","Theodor","Sever"]
-race = [
-"Dragonborn",
-"Dwarf",
-"Elf",
-"Gnome",
-"Half-Elf",
-"Halfling",
-"Half-Orc",
-"Human",
-"Tiefling",
-"Changeling",
-"Kalashtar",
-"Shifter",
-"Warforged",
-"Aarakocra",
-"Genasi",
-"Goliath",
-"Centaur",
-"Loxodon",
-"Minotaur",
-"Simic Hybrid",
-"Vedalken",
-"Aetherborn",
-"Aven",
-"Khenra",
-"Kor",
-"Merfolk",
-"Naga",
-"Siren",
-"Vampire",
-"Aasimar",
-"Bugbear",
-"Firbolg",
-"Goblin",
-"Hobgoblin",
-"Kenku",
-"Kobold",
-"Lizardfolk",
-"Orc",
-"Tabaxi",
-"Triton",
-"Yuan-ti Pureblood",
-"Gith",
-"Locathah",
-"Tortle",
-"Verdan "
-]
 
+
+race = json.loads(open('racesDnd.json').read())
 rollRace = int(random.randint(0,len(race)-1))
-rollParty = int(random.randint(0,len(party)-1))
 
-print(str("\n")+str(party[rollParty])+str(" has been turned into a ")+str(race[rollRace])) 
+print(race[rollRace])
